35b01840caecdb916b1af4f76cad6303cd34c727
Rémi Rubis <remi.rubis@yahoo.com>
2023-04-25 22:35:38 +0200

    refactor(parts/Header-Footer): styling

--- app/src/ui/parts/Header/Header.tsx
+++ app/src/ui/parts/Header/Header.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Header.scss";
+
 export const Header: React.FC = () => (
   <header>
-    <h1>React PWA</h1>
-    <nav>
-      <Link to="/">Home</Link>
-    </nav>
+    <div className="header--content">
+      <h1>React PWA</h1>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </div>
   </header>
 );
